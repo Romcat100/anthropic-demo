7df5357ba0277ab2075101239b9a3720d367a43f
Ben Romney <bromney@qualtrics.com>
2022-11-16 18:02:11 -0700

    Anthropic demo code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div id='top-nav'>
+          <span>Demo</span>
+        </div>
+        <div id='main-body'>
+          <div id='controls'>
+            <div>
+              <h2>Controls</h2>
+              <div>
+                <label for='fruit-select'>Favorite fruit </label>
+                <select id='fruit-select'>
+                  <option>Banana</option>
+                  <option>Lime</option>
+                  <option>Mango</option>
+                </select>
+              </div>
+              <div>
+                <label for='vegetable-select'>Favorite vegetable </label>
+                <input type='text' id='vegetable-select'></input>
+              </div>
+              <div>
+                <label for='bread-select'>Favorite bread </label>
+                <input type='text' id='bread-select'></input>
+              </div>
+            </div>
+          </div>
+          <div id='composer'>
+            <div>
+              <div id='text-gen' contentEditable='true'></div>
+            </div>
+            <button onClick={generateText}>
+              Generate text
+            </button>
+          </div>
+        </div>
     </div>
   );
+}
+
+function generateText() {
+  const currText = document.getElementById('text-gen').innerHTML
+  const text = ' lorem ipsum ';
+  const newHtml = `<span class='highlight'>${ text }</span>`;
+  document.getElementById('text-gen').innerHTML = `${ currText } ${ newHtml }`;
 }
 
 export default App;
